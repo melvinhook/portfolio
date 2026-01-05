b3a32261ed8f8430bcba588a0811590575d0de73
scav <hookadoo24@gmail.com>
2026-01-05 10:45:52 +0700

    First template, One Page, ONLY LANDING PAGE, NO ANIMATION

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css";
+import bg from "./assets/Fuji.jpg";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="h-full w-full">
+        <div className="absolute h-screen text-white flex flex-col justify-center ml-5">
+            <div className="h-80 w-0.5 border bg-white"></div>
+            <h1 className="mt-5">1 ページ</h1>
+            <div className="h-80 w-0.5 border bg-white mt-5"></div>
+        </div>
+        <div className="absolute z-10 h-full w-full flex items-center justify-center text-white text-[25px] text-center">
+          <div>
+            <p>
+              阿<br />久<br />世<br />流
+            </p>
+            <div className="border border-white w-15 bg-white mt-[5%]"></div>
+            <p className="text-[15px]">Axel</p>
+          </div>
+        </div>
+        <img src={bg} className="h-full w-full object-cover" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
