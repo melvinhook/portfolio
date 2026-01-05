7f6ea46cad741ae9a5c1e1266b3aeab7e4128956
scav <hookadoo24@gmail.com>
2026-01-05 16:48:41 +0700

    Personal Records 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,103 @@
 import "./App.css";
-import bg from "./assets/Fuji.jpg"; 
-import { useRef, useEffect, useState} from "react";  
-import { useSpring, animated } from "@react-spring/web";
-function App() { 
-  const div4 = useRef(null)   
-  const [mt,setMt] = useState(300)
-  const container = useSpring({
-    to:{
-      marginTop: `${mt}vh`
-    }
-  })    
-  const pitchBlack = useSpring({
+import bg from "./assets/Fuji.jpg";
+import { useRef, useEffect, useState } from "react";
+import { useSpring, animated, a } from "@react-spring/web";
+function App() {
+  const div4 = useRef(null);
+  const [mt, setMt] = useState(300);
+  const [doSomething, setDoSomething] = useState(false);
+  const [lock, setLock] = useState(false); 
+  const [title,titleApi] = useSpring(()=>({
     from:{
-      opacity: 1
+      opacity:0
     }, 
-    to:{
-      opacity: 0
+  }))   
+  const [upperLiner,upperLinerApi] = useState(()=>({
+    from:{
+      
     }
-  }) 
-  useEffect(()=>{
+  }))
+  
+  const container = useSpring({
+    to: {
+      marginTop: `${mt}vh`,
+    },
+    onRest: () => {
+      setLock(false);
+    },
+    config: {
+      mass: 15,
+      tension: 65,
+      friction: 50,
+    },
+  });
+  const pitchBlack = useSpring({
+    from: {
+      opacity: 1,
+    },
+    to: {
+      opacity: 0,
+    },
+    config: {
+      mass: 15,
+      tension: 65,
+      friction: 50,
+    }, 
+    onRest:()=>{
+      titleApi.start({
+        to:{opacity:1}
+      })
+    }
+  });
+  useEffect(() => {
+    if (!doSomething) return;
+    const handleWheel = (e) => {
+      if (lock) return;
+      setLock(true);
+      if (e.deltaY > 0) {
+        setMt((prev) => prev - 100);
+        console.log("down");
+      } else {
+        setMt((prev) => prev + 100);
+        console.log("up");
+      }
+    };
+    window.addEventListener("wheel", handleWheel, { passive: true });
+    return () => window.removeEventListener("wheel", handleWheel);
+  }, [doSomething, lock]);
+  useEffect(() => {
     if (div4.current) {
       div4.current.scrollIntoView();
+      setDoSomething(true);
     }
-  },[])
+  }, []);
   return (
     <>
-      <section className="h-[600vh] w-full"> 
-        <animated.div style={pitchBlack} className="absolute z-10 h-[600vh] w-full bg-black"></animated.div>
-        <div className="h-screen w-full mt-[300vh] absolute">
-          <div className="absolute h-screen text-white flex flex-col justify-center ml-5">
-            <div className="2xl:h-80 xl:h-60 lg:h-60 md:h-50 w-0.5 border bg-white"></div>
-            <h1 className="mt-5">1 ページ</h1>
-            <div className="2xl:h-80 xl:h-60 lg:h-60 md:h-50 w-0.5 border bg-white mt-5"></div>
-          </div>
-          <div className="absolute z-10 h-full w-full flex items-center justify-center text-white text-[25px] text-center">
-            <div>
-              <p>
-                阿<br/>久<br/>世<br/>流
-              </p>
-              <div className="border border-white w-15 bg-white mt-[5%]"></div>
-              <p className="text-[15px]">Axel</p>
+      <section className="h-[600vh] w-full">
+        <animated.div
+          style={pitchBlack}
+          className="absolute z-10 h-[600vh] w-full bg-black"
+        ></animated.div>
+        <animated.div style={container} className="absolute">
+          <div className="h-screen w-screen">
+            <div className="absolute h-screen text-white flex flex-col justify-center ml-5">
+              <div className="h-[40%] w-0.5 border bg-white"></div>
+              <h1 className="mt-5">1 ページ</h1>
+              <div className="h-[40%] w-0.5 border bg-white mt-5"></div>
             </div>
+            <animated.div style={title} className="absolute  h-screen w-screen flex items-center justify-center text-white text-[25px] text-center">
+              <div>
+                <p>
+                  阿<br />久<br />世<br />流
+                </p>
+                <div className="border border-white w-15 bg-white mt-[5%]"></div>
+                <p className="text-[15px]">Axel</p>
+              </div>
+            </animated.div>
+            <img src={bg} className="h-full w-full object-cover" />
           </div>
-          <img src={bg} className="h-full w-full object-cover" />
-        </div>
+          <div className="h-screen w-screen bg-[#1b1f1e]"></div>
+        </animated.div>
         <div className="h-screen w-screen flex items-center justify-center italic border">
           FREE-DIV-1
         </div>
@@ -53,7 +107,10 @@ function App() {
         <div className="h-screen w-screen flex items-center justify-center italic border">
           FREE-DIV-3
         </div>
-        <div ref={div4} className="h-screen w-screen flex items-center justify-center italic border">
+        <div
+          ref={div4}
+          className="h-screen w-screen flex items-center justify-center italic border"
+        >
           FREE-DIV-4
         </div>
         <div className="h-screen w-screen flex items-center justify-center italic border">
