f3b4c2ae689e085926c9e6fa7868ec26cd71e232
scav <hookadoo24@gmail.com>
2026-01-15 16:36:49 +0700

    New Portfolios

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "../app.css";
 import Section1 from "./components/section1";
 import Section2 from "./components/section2"; 
 import Section3 from "./components/section3"; 
