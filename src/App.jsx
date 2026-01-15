01b6ef1f5c4e0783584d59fbed54f6831a84a917
scav <hookadoo24@gmail.com>
2026-01-15 21:56:56 +0700

    NEW PORT ONEss

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./app.css";
+import "../app.css";
 import Section1 from "./components/section1";
 import Section2 from "./components/section2"; 
 import Section3 from "./components/section3"; 
